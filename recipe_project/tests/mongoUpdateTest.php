d9737d6d702bcae52aa5aa55b21ec9695e95396c
Alex Figueroa <agf3@njit.edu>
2011-08-01 19:28:47 -0400

    Created update script to make sure that the update function was working

--- recipe_project/tests/mongoUpdateTest.php
+++ recipe_project/tests/mongoUpdateTest.php
@@ -2,9 +2,31 @@
 
 include("../classes/class_thing_lib.php");
 
-$thing = new Thing();
+/*Shows all three parts of the update process.
+  1) Grab Original values from DB
+  2) Insert new values into DB
+  3) Verify that the values were upadted */
 
-/*All different update functions*/
-$thing->mongo_to_thing('localhost', 'Recipe', '4e34771f6b26e6e144020000');
-print_r($thing->print_schema());
+
+/*Show what the original schema looked like*/
+echo "<h1>BEFORE UPDATE</h1>";
+$originalThing = new Thing();
+$originalThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+print_r($originalThing->print_schema());
+
+/*Show what the update should look like*/
+echo "</br><h1>THE UPDATE</h1>";
+$updatedThing = new Thing();
+$updatedThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+$updatedThing->set_name("Cricket 2.0");
+$updatedThing->set_description("Still haven't played it");
+$updatedThing->set_image("http://graphics8.nytimes.com/images/2009/08/09/sports/cricket/09cricket-600.jpg");
+$updatedThing->update_to_mongo('localhost', 'Recipe');
+print_r($updatedThing->print_schema());
+
+/*Show what the update actually looks like (Should be same as above)*/
+echo "</br><h1>AFTER THE UPDATE</h1>";
+$newThing = new Thing();
+$newThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+print_r($newThing->print_schema());
 ?>
