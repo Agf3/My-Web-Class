6c77c4b493d339f8b81206c165aeeb3435857725
Alex Figueroa <agf3@njit.edu>
2011-08-01 22:36:58 -0400

    Added printing functions to make sure it was entering the db correctly

--- recipe_project/tests/mongoUpdateTest.php
+++ recipe_project/tests/mongoUpdateTest.php
@@ -11,13 +11,13 @@ include("../classes/class_thing_lib.php");
 /*Show what the original schema looked like*/
 echo "<h1>BEFORE UPDATE</h1>";
 $originalThing = new Thing();
-$originalThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+$originalThing->mongo_to_thing('localhost', 'Recipe', '4e373d191ce31e201e000000');
 print_r($originalThing->print_schema());
 
 /*Show what the update should look like*/
 echo "</br><h1>THE UPDATE</h1>";
 $updatedThing = new Thing();
-$updatedThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+$updatedThing->mongo_to_thing('localhost', 'Recipe', '4e373d191ce31e201e000000');
 $updatedThing->set_name("Cricket 2.0");
 $updatedThing->set_description("Still haven't played it");
 $updatedThing->set_image("http://graphics8.nytimes.com/images/2009/08/09/sports/cricket/09cricket-600.jpg");
@@ -27,6 +27,6 @@ print_r($updatedThing->print_schema());
 /*Show what the update actually looks like (Should be same as above)*/
 echo "</br><h1>AFTER THE UPDATE</h1>";
 $newThing = new Thing();
-$newThing->mongo_to_thing('localhost', 'Recipe', '4e37362a6b26e60020050000');
+$newThing->mongo_to_thing('localhost', 'Recipe', '4e373d191ce31e201e000000');
 print_r($newThing->print_schema());
 ?>
