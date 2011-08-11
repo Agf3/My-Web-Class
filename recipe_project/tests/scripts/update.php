866c3bb48e0d71125f30bf46bb13b558ae18fe64
Alex Figueroa <agf3@njit.edu>
2011-08-10 22:46:40 -0400

    Added Recipe fields

--- recipe_project/tests/scripts/update.php
+++ recipe_project/tests/scripts/update.php
@@ -1,31 +1,46 @@
 <?php
 
 $id = $_POST['ID'];
+
 $name = $_POST['Name'];
 $description = $_POST['Description'];
 $image = $_POST['Image'];
 $url = $_POST['Url'];
 
-include("../../classes/class_thing_lib.php");
+$headline = $_POST['Headline'];
+$author = $_POST['Author'];
+$awards = $_POST['Awards'];
+$keywords = $_Post['Keywords'];
+
+$recipeCategory = $_POST['RecipeCategory'];
+$recipeCuisine = $_POST['RecipeCuisine'];
+$cookingMethod = $_POST['CookingMethod'];
+$ingredients = $_POST['Ingredients'];
+$prepTime = $_POST['PrepTime'];
+$cookTime = $_POST['CookTime'];
+$totalTime = $_POST['TotalTime'];
+$recipeYield = $_POST['RecipeYield'];
+$recipeInstructions = $_POST['RecipeInstructions'];
+
+include("../../classes/class_recipe_lib.php");
 
 /*Show what the original schema looked like*/
 echo "<h1>ORIGINAL HTML & SCHEMA</h1>";
-$originalThing = new Thing();
-$originalThing->mongo_to_thing('localhost', 'Recipe', $id);
+$originalThing = new Recipe();
+$originalThing->mongo_to_array('localhost', 'Recipe', $id);
 print_r($originalThing->print_schema());
 echo "</br>";
 print_r($originalThing->retrieve_from_mongo_id('localhost', 'Recipe', $originalThing->get_id()));
 
 /*Show what the update looks like*/
 echo "</br><h1>THE UPDATED HTML & SCHEMA</h1>";
-$updatedThing = new Thing();
-$updatedThing->mongo_to_thing('localhost', 'Recipe', $id);
-$updatedThing->set_name($name);
-$updatedThing->set_description($description);
-$updatedThing->set_image($image);
+$updatedThing = new Recipe();
+$updatedThing->mongo_to_array('localhost', 'Recipe', $id);
+
+$updatedThing->set_all($name, $description, $image, $url, $headline, $author, $awards, $keywords, $recipeCategory, $recipeCuisine, $cookingMethod, $prepTime, $cookTime, $totalTime, $recipeYield, $ingredients, $recipeInstructions);
 $updatedThing->update_to_mongo('localhost', 'Recipe');
 print_r($updatedThing->print_schema());
 echo "</br>";
-print_r($updatedThing->retrieve_from_mongo_id('localhost', 'Recipe', $originalThing->get_id()));
+print_r($updatedThing->retrieve_from_mongo_id('localhost', 'Recipe', $updatedThing->get_id()));
 
 ?>
--- recipe_project/tests/scripts/updateScript.php
+++ recipe_project/tests/scripts/updateScript.php
@@ -7,22 +7,40 @@
 
 <body>
 <?php
-include("../../classes/class_thing_lib.php");
+include("../../classes/class_recipe_lib.php");
 
 $id = $_POST['ID'];
 //echo $id;
 
-$thing = new Thing();
-$thing->mongo_to_thing('localhost', 'Recipe', $id);
+$thing = new Recipe();
+$thing->mongo_to_array('localhost', 'Recipe', $id);
+
+/*Retrieve all the values for the form*/
 $name = $thing->get_name();
 $description = $thing->get_description();
 $image = $thing->get_image();
 $url = $thing->get_url();
 
+$headline = $thing->get_headline();
+$author = $thing->get_author();
+$awards = $thing->get_awards();
+$keywords = $thing->get_keywords();
+
+$recipeCategory = $thing->get_recipe_category();
+$recipeCuisine = $thing->get_recipe_cuisine();
+$cookingMethod = $thing->get_cooking_method();
+$ingredients = $thing->get_ingredients();
+$prepTime = $thing->get_prep_time();
+$cookTime = $thing->get_cook_time();
+$totalTime = $thing->get_total_time();
+$recipeYield = $thing->get_recipe_yield();
+$recipeInstructions = $thing->get_recipe_instructions();
+
 ?>
-<h1 align="center">UPDATE YOUR THING</h1>
+<h1 align="center">UPDATE YOUR RECIPE</h1>
 <form action="update.php" method="post">
 <div align="center">
+<input name="ID" type="hidden" value="<?php echo $id; ?>" />
   <h2 align="center"><strong>Name:</strong></h2>
   <input name="Name" type="text" size="100" value="<?php echo $name; ?>"/>
  <h2><strong>Description:</strong></h2>
@@ -31,9 +49,34 @@ $url = $thing->get_url();
   <input name="Image" type="text" size="100" value="<?php echo $image; ?>"/>
 <h2><strong>URL:</strong></h2>
   <input name="Url" type="text" size="100" value="<?php echo $url; ?>"/>
-  <input name="ID" type="hidden" value="<?php echo $id; ?>" />
+  <h2><strong>Headline:</strong></h2>
+  <input name="Headline" type="text" size="100" value="<?php echo $headline; ?>"/>
+  <h2><strong>Author:</strong></h2>
+  <input name="Author" type="text" size="100" value="<?php echo $author; ?>"/>
+  <h2><strong>Awards:</strong></h2>
+  <input name="Awards" type="text" size="100" value="<?php echo $awards; ?>"/>
+  <h2><strong>Keywords:</strong></h2>
+  <input name="Keywords" type="text" size="100" value="<?php echo $keywords; ?>"/>
+  <h2><strong>Recipe Category:</strong></h2>
+  <input name="RecipeCategory" type="text" size="100" value="<?php echo $recipeCategory; ?>"/>
+  <h2><strong>Recipe Cuisine:</strong></h2>
+  <input name="RecipeCuisine" type="text" size="100" value="<?php echo $recipeCuisine; ?>"/>
+  <h2><strong>Cooking Method:</strong></h2>
+  <input name="CookingMethod" type="text" size="100" value="<?php echo $cookingMethod; ?>"/>
+  <h2><strong>Ingredients:</strong></h2>
+  <input name="Ingredients" type="text" size="100" value="<?php echo $ingredients; ?>"/>
+  <h2><strong>Prep Time:</strong></h2>
+  <input name="PrepTime" type="text" size="100" value="<?php echo $prepTime; ?>"/>
+  <h2><strong>Cook Time:</strong></h2>
+  <input name="CookTime" type="text" size="100" value="<?php echo $cookTime; ?>"/>
+  <h2><strong>Total Time:</strong></h2>
+  <input name="TotalTime" type="text" size="100" value="<?php echo $totalTime; ?>"/>
+  <h2><strong>Recipe Yield:</strong></h2>
+  <input name="RecipeYield" type="text" size="100" value="<?php echo $recipeYield; ?>"/>
+  <h2><strong>Recipe Instructions:</strong></h2>
+  <input name="RecipeInstructions" type="text" size="100" value="<?php echo $recipeInstructions; ?>"/>
   <p></br></p>
-<input name="submit" type="submit" value="UPDATE THING" />
+<input name="submit" type="submit" value="UPDATE RECIPE" />
 </div>
 </form>
 </body>
